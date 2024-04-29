ce09c0a8dcf9e4e3b830877623f2916f561fa707
Weng Weiyu <weiyu.weng@u.nus.edu>
2024-04-29 18:12:15 +0800

    Update Taiko.sh

--- Taiko.sh
+++ Taiko.sh
@@ -148,49 +148,6 @@ sed -i "s|BLOCK_PROPOSAL_FEE=.*|BLOCK_PROPOSAL_FEE=30|" .env
 # 用户信息已配置完毕
 echo "用户信息已配置完毕。"
 
-# 升级所有已安装的包
-sudo apt upgrade -y
-
-# 安装基本组件
-sudo apt install pkg-config curl build-essential libssl-dev libclang-dev ufw docker-compose-plugin -y
-
-# 检查 Docker 是否已安装
-if ! command -v docker &> /dev/null
-then
-    # 如果 Docker 未安装，则进行安装
-    echo "未检测到 Docker，正在安装..."
-    sudo apt-get install ca-certificates curl gnupg lsb-release
-
-    # 添加 Docker 官方 GPG 密钥
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-    # 设置 Docker 仓库
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    # 授权 Docker 文件
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-    sudo apt-get update
-
-    # 安装 Docker 最新版本
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-else
-    echo "Docker 已安装。"
-fi
-
-    # 安装 Docker compose 最新版本
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-docker compose version
-
-# 验证 Docker Engine 安装是否成功
-sudo docker run hello-world
-# 应该能看到 hello-world 程序的输出
-
 # 运行 Taiko 节点
 docker compose --profile l2_execution_engine down
 docker stop simple-taiko-node-taiko_client_proposer-1 && docker rm simple-taiko-node-taiko_client_proposer-1
